611cd36eb203b36dee1679189975b1fbd614611a
Sebastian  Lesmes <sebastianj.lesmesm@uqvirtual.edu.co>
2025-02-07 21:52:47 -0500

    Constructor

---
+++ SG1/Programa.java
@@ -0,0 +1,15 @@
+public class Programa {
+    private Enum facultad;
+    private int codigo;
+    private String nombre;
+    private int numeroCreditos;
+    private Enum enumCreditos;
+    
+    public Programa(Enum facultad, int codigo, String nombre, int numeroCreditos, Enum enumCreditos) {
+        this.facultad = facultad;
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.numeroCreditos = numeroCreditos;
+        this.enumCreditos = enumCreditos;
+    }
+
